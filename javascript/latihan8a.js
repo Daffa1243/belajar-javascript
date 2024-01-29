e3a25319e23cdea43b9fc19b3cd1f1a3dad30543
Daffa Akhdan Permana <daffapermana885@gmail.com>
2024-01-29 15:14:12 +0700

    latihan bang

---
+++ javascript/latihan8a.js
@@ -0,0 +1,15 @@
+var nilai = prompt ("masukan nilai :")
+
+ if(nilai <= 39) 
+    alert(nilai + "= grade E");
+ else if(nilai <=64) 
+    alert(nilai + "= grade D")
+    else if(nilai <=74) 
+        alert(nilai + "= grade c") 
+        else if(nilai <=84) 
+            alert(nilai + "= grade B")
+            else if(nilai <=100) 
+                alert(nilai + "= grade A")
+            else {
+                alert ("data bukan angka")
+            }
